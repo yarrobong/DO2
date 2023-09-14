8b8bc06849db2f03199ed379f419959912abbdac
yarrobong <oneboardsholi@gmail.com>
2023-09-14 22:41:38 +0500

    Добавьте файлы проекта.

---
+++ DO2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число: ");
+var x = Console.ReadLine();
+Console.WriteLine("Введите второе  число: ");
+var y = Console.ReadLine();
+Console.WriteLine($"Теперь первое число равно {x}, а второе равно {y}");
+
+
+
